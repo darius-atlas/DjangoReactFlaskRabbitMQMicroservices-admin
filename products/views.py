6a400f1c971bb7c7399e52ad1041d69d72e66fe8
Darius Artem Atlas <31390399+darius-atlas@users.noreply.github.com>
2021-07-23 16:57:12 -0700

    added create api in the controller

--- products/views.py
+++ products/views.py
@@ -13,7 +13,10 @@ class ProductViewSet(viewsets.ViewSet):
         return Response(serializer.data)
 
     def create(self, request):
-        pass
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def retrieve(self, request, pk=None):
         pass
