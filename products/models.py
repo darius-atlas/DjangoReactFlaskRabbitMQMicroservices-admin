173b9ae6298dbe2739f614fbea418aca6e4a42a4
Darius Artem Atlas <31390399+darius-atlas@users.noreply.github.com>
2021-07-23 15:08:05 -0700

    Update models.py

--- products/models.py
+++ products/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Product(models.Model):
     title = models.CharField(max_length=200)
     image = models.CharField(max_length=200)
-    likes = models.PositiveIntegerFiels(default=0)
+    likes = models.PositiveIntegerField(default=0)
 
 class User(models.Model):
     pass
